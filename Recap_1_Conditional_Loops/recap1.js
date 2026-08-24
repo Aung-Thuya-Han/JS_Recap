25912ea6b1218ebb109f674b1b34f708abff85e2
aungthuyahan <aungthu.ya659@gmail.com>
2026-08-24 19:00:35 +0300

    first commit

---
+++ Recap_1_Conditional_Loops/recap1.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const celsius = parseFloat(prompt('Enter the temperature in celsius: '));
+const fahrenheit = ((celsius * 9) / 5 ) + 32;
+const kelvin = celsius + 273.15;
+
+document.querySelector('#answers').innerHTML = `
+Temperature in Celsius: ${celsius} 
+<br/>
+Temperature in Fahrenheit: ${fahrenheit}
+<br/>
+Temperature in Kelvin: ${kelvin}
+`;
