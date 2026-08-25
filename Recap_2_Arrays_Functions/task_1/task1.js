ce1ddfce1980aabec8883136dd8931c8fb44701e
aungthuyahan <aungthu.ya659@gmail.com>
2026-08-25 18:49:01 +0300

    fix: correct script tag typo in task1.html and add initial code to task1.js for fruit and vegetable logging

--- Recap_2_Arrays_Functions/task_1/task1.js
+++ Recap_2_Arrays_Functions/task_1/task1.js
@@ -1,2 +1,21 @@
 'use strict';
 
+const fruits = ['apple', 'banana', 'orange', 'grape', 'kiwi'];
+
+console.log('Fruits:', fruits);
+console.log('Length of fruits:', fruits.length);
+console.log('Element at index 2:', fruits[2]);
+console.log('Last Element of Fruits:', fruits[fruits.length - 1]);
+
+
+const vegetables = [];
+
+for (let i = 1; i <= 3; i++) {
+    const vegetableName = prompt('Enter a vegetable;');
+
+    vegetables.push(vegetableName);
+}
+
+console.log('Vegetables;', vegetables);
+console.log('Length of Vegetables:', vegetables.length);
+
